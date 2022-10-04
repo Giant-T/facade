1bc1b09e4e96c385cfaa2e062b79490bcd101de7
William Boudreault <boudreaultwilliam@gmail.com>
2022-10-04 19:54:59 -0400

    Changement de gitignore car mauvais

---
+++ Facade.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Facade.Lib;
+
+namespace Facade.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FacadeService facade = new FacadeService();
+            int idDemande = facade.AcheterService(20, "decoupe de feuille d'acier", "William");
+
+            System.Console.WriteLine("AH non! Je ne veux plus ma decoupe");
+            
+            facade.AnnulerService(idDemande);
+        }
+    }
+}
